60a048b71be3d30b3dcc3eb400557d13715adb5b
xjinw <xjinw@outlook.com>
2018-07-01 15:36:02 +0800

    finish login control

--- base.js
+++ base.js
@@ -1,31 +1,44 @@
-const BASEURL = "http://123.206.87.13:8081/";
-// const BASEURL = "http://localhost:8081/";
+// const BASEURL = "http://123.206.87.13:8081/";
+const BASEURL = "http://localhost:8081/";
+
+//导航条判断渲染
 $(document).ready(function(){
-    $.get(BASEURL + "public/loginCheck", function(result, status){
-        var appendStr = "";
-        console.log(result);
-        if (result.code == 0) {
-            appendStr = `
-            <li class="layui-nav-item">
-                <a href="javascript:;">${result.data}</a>
-                <dl class="layui-nav-child">
-                <dd><a href="">个人资料</a></dd>
-                <dd><a href="">我的项目</a></dd>
-                <dd><a href="">通知消息</a></dd>
-                <dd><a href="">注销</a></dd>
-                </dl>
-            </li>
-            `;
-        }else {
-            appendStr = `
-            <li class="layui-nav-item">
-              <a href="login.html">登录</a>
-            </li>
-            <li class="layui-nav-item">
-              <a href="register.html">注册</a>
-            </li>
-            `;
+    $.ajax({
+        type: "GET",
+        url: BASEURL + "public/loginCheck",
+        xhrFields: { withCredentials: true},
+        success: function(result, status) {
+            if (result.code == 0) {
+                appendStr = `
+                <li class="layui-nav-item">
+                <a href="login.html">个人中心</a>
+                </li>
+                <li class="layui-nav-item">
+                <a id="logout">注销</a>
+                </li>
+                `;
+            }else{
+                appendStr = `
+                <li class="layui-nav-item">
+                <a href="login.html">登录</a>
+                </li>
+                <li class="layui-nav-item">
+                <a href="register.html">注册</a>
+                </li>
+                `;
+            }
+            $("#nav").append(appendStr);
+            $("#logout").on('click',function(){
+                $.ajax({
+                    type: "GET",
+                    url: BASEURL + "public/logout",
+                    xhrFields: { withCredentials: true},
+                    success: function(result, status) {
+                        if (result.code == 0) 
+                            window.location.href = "index.html";
+                    }
+                });
+            });
         }
-        $(".layui-nav").append(appendStr);
     });
 });
